bcd08b024c62a5454013b5dba27fafeb3ea5fa4d
twistzzmc <michal.szczepaniak1999@gmail.com>
2025-03-20 21:51:18 +0100

    Pierwsze GUI z wypisaniem przeanalizowanej klasy

--- CodeAnalyzer/Program.cs
+++ CodeAnalyzer/Program.cs
@@ -1,19 +1,38 @@
 ﻿
+using Avalonia;
 using CodeAnalyzer;
 using CodeAnalyzer.Core.Models;
 using CodeAnalyzer.Core.Warnings;
 using CodeAnalyzer.Parser;
+using CodeAnalyzer.UI;
 
-// const string classFilePath = "/home/michal-szczepaniak/mleko/code-examples/NLog/src/NLog/Logger.cs";
-const string classFilePath = "/home/michal-szczepaniak/mleko/code-examples/NLog/tests/NLog.UnitTests/Targets/FileTargetTests.cs";
-// const string classFilePath = "/home/michal-szczepaniak/mleko/code-examples/NLog/src/NLog/Targets/AsyncTaskTarget.cs";
-// const string classFilePath = "/home/michal-szczepaniak/mleko/code-examples/NLog/src/NLog/Common/InternalLogger.cs";
+// // const string classFilePath = "/home/michal-szczepaniak/mleko/code-examples/NLog/src/NLog/Logger.cs";
+// const string classFilePath = "/home/michal-szczepaniak/mleko/code-examples/NLog/tests/NLog.UnitTests/Targets/FileTargetTests.cs";
+// // const string classFilePath = "/home/michal-szczepaniak/mleko/code-examples/NLog/src/NLog/Targets/AsyncTaskTarget.cs";
+// // const string classFilePath = "/home/michal-szczepaniak/mleko/code-examples/NLog/src/NLog/Common/InternalLogger.cs";
+//
+// WarningRegistry registry = new();
+// registry.OnWarning += WarningLogger.Log;
+//
+// string code = File.ReadAllText(classFilePath);
+// ClassModel model = CodeParser.Parse(registry, code);
+//
+// Console.WriteLine(model.ToString());
+// model.PrintTooLongMethods();
 
-WarningRegistry registry = new();
-registry.OnWarning += WarningLogger.Log;
+namespace CodeAnalyzer;
 
-string code = File.ReadAllText(classFilePath);
-ClassModel model = CodeParser.Parse(registry, code);
+public static class Program
+{
+    public static void Main(string[] args)
+    {
+        BuildAvaloniaApp()
+            .StartWithClassicDesktopLifetime([]);
+    }
 
-Console.WriteLine(model.ToString());
-model.PrintTooLongMethods();
+    private static AppBuilder BuildAvaloniaApp()
+        => AppBuilder.Configure<App>()
+            .UsePlatformDetect()
+            .WithInterFont()
+            .LogToTrace();
+}
