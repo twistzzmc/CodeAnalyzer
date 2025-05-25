51ca4d6b3b45b041f943f42e78d262ca6f614a89
twistzzmc <michal.szczepaniak1999@gmail.com>
2025-05-25 15:44:35 +0200

    Refaktor ReferencesCalculator.cs

---
+++ CodeAnalyzer.Parser/Collectors/Calculators/Base/BaseReferenceCalculator.cs
@@ -0,0 +1,59 @@
+using CodeAnalyzer.Core.Models.SubModels;
+using CodeAnalyzer.Core.Warnings.Interfaces;
+using CodeAnalyzer.Parser.Collectors.Creators;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+
+namespace CodeAnalyzer.Parser.Collectors.Calculators.Base;
+
+internal abstract class BaseReferenceCalculator(IWarningRegistry warningRegistry, CSharpCompilation compilation)
+{
+    protected delegate bool IsReferenceDelegate<in TInvocation>(
+        TInvocation invocation,
+        SemanticModel treeModel,
+        ISymbol? memberSymbol) where TInvocation : CSharpSyntaxNode;
+    
+    private readonly NamespaceCreator _namespaceCreator = new(warningRegistry) { ExpectNonNamespaceDeclarations = true };
+
+    protected IEnumerable<ReferenceInstance> BaseCalculate<TInvocation>(
+        CSharpSyntaxNode node,
+        IsReferenceDelegate<TInvocation> isReference)
+            where TInvocation : CSharpSyntaxNode
+    {
+        SemanticModel semanticModel = compilation.GetSemanticModel(node.SyntaxTree);
+        ISymbol? memberSymbol = semanticModel.GetDeclaredSymbol(node);
+
+        if (memberSymbol is null)
+        {
+            return [];
+        }
+        
+        List<ReferenceInstance> references = [];
+        foreach (SyntaxTree tree in compilation.SyntaxTrees)
+        {
+            SemanticModel treeModel = compilation.GetSemanticModel(tree);
+            SyntaxNode root = tree.GetRoot();
+            IEnumerable<TInvocation> invocations = root.DescendantNodes().OfType<TInvocation>();
+
+            foreach (TInvocation invocation in invocations)
+            {
+                if (isReference(invocation, treeModel, memberSymbol))
+                {
+                    references.Add(CreateReference(invocation));
+                }
+            }
+        }
+        
+        return references;
+    }
+
+    private ReferenceInstance CreateReference(CSharpSyntaxNode reference)
+    {
+        return new ReferenceInstance
+        {
+            Namespace = _namespaceCreator.CreateJoined(reference),
+            LineNumber = reference.GetLocation().GetLineSpan().StartLinePosition.Line + 1,
+            ColumnNumber = reference.GetLocation().GetLineSpan().StartLinePosition.Character + 1
+        };
+    }
+}
--- CodeAnalyzer.Parser/Collectors/Calculators/ReferencesCalculator.cs
+++ CodeAnalyzer.Parser/Collectors/Calculators/ReferencesCalculator.cs
@@ -1,7 +1,6 @@
 using CodeAnalyzer.Core.Models.SubModels;
-using CodeAnalyzer.Core.Models.SubModels.PropertyValues;
 using CodeAnalyzer.Core.Warnings.Interfaces;
-using CodeAnalyzer.Parser.Collectors.Creators;
+using CodeAnalyzer.Parser.Collectors.Calculators.Base;
 using CodeAnalyzer.Parser.Collectors.Interfaces;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp;
@@ -10,118 +9,46 @@ using Microsoft.CodeAnalysis.CSharp.Syntax;
 namespace CodeAnalyzer.Parser.Collectors.Calculators;
 
 internal sealed class ReferencesCalculator(IWarningRegistry warningRegistry, CSharpCompilation compilation) :
+    BaseReferenceCalculator(warningRegistry, compilation),
     ICalculator<IEnumerable<ReferenceInstance>, MethodDeclarationSyntax>,
     ICalculator<IEnumerable<ReferenceInstance>, PropertyDeclarationSyntax>,
     ICalculator<IEnumerable<ReferenceInstance>, VariableDeclaratorSyntax>
 {
-    private readonly NamespaceCreator _namespaceCreator = new(warningRegistry) { ExpectNonNamespaceDeclarations = true };
-    
+
     public IEnumerable<ReferenceInstance> Calculate(MethodDeclarationSyntax options)
     {
-        SemanticModel semanticModel = compilation.GetSemanticModel(options.SyntaxTree);
-        IMethodSymbol? memberSymbol = semanticModel.GetDeclaredSymbol(options);
-        List<ReferenceInstance> references = [];
-        
-        foreach (SyntaxTree tree in compilation.SyntaxTrees)
-        {
-            SemanticModel treeModel = compilation.GetSemanticModel(tree);
-            SyntaxNode root = tree.GetRoot();
-            
-            IEnumerable<InvocationExpressionSyntax> invocations = root.DescendantNodes()
-                .OfType<InvocationExpressionSyntax>();
-        
-            foreach (InvocationExpressionSyntax invocation in invocations)
-            {
-                SymbolInfo invocationInfo = treeModel.GetSymbolInfo(invocation);
-                ISymbol? invocationSymbol = invocationInfo.Symbol ?? invocationInfo.CandidateSymbols.FirstOrDefault();
-
-                if (invocationSymbol is IMethodSymbol invokedMethod &&
-                    SymbolEqualityComparer.Default.Equals(invokedMethod.OriginalDefinition,
-                        memberSymbol?.OriginalDefinition))
-                {
-                    references.Add(CreateReference(invocation));
-                }
-            }
-        }
-        
-        return references;
+        return BaseCalculate<InvocationExpressionSyntax>(options, IsMethodReference);
     }
 
     public IEnumerable<ReferenceInstance> Calculate(PropertyDeclarationSyntax options)
     {
-        SemanticModel propertySemanticModel = compilation.GetSemanticModel(options.SyntaxTree);
-        ISymbol? propertySymbol = propertySemanticModel.GetDeclaredSymbol(options);
-
-        if (propertySymbol == null)
-        {
-            return [];
-        }
-        
-        List<ReferenceInstance> references = [];
-        foreach (SyntaxTree tree in compilation.SyntaxTrees)
-        {
-            SemanticModel treeModel = compilation.GetSemanticModel(tree);
-            SyntaxNode treeRoot = tree.GetRoot();
-        
-            IEnumerable<IdentifierNameSyntax> identifiers = treeRoot.DescendantNodes()
-                .OfType<IdentifierNameSyntax>();
-
-            foreach (IdentifierNameSyntax identifier in identifiers)
-            {
-                ISymbol? treeIdentifierSymbol = treeModel.GetSymbolInfo(identifier).Symbol;
-                if (!SymbolEqualityComparer.Default.Equals(treeIdentifierSymbol, propertySymbol))
-                {
-                    continue;
-                }
-                
-                references.Add(CreateReference(identifier));
-            }
-        }
-
-        return references;
+        return BaseCalculate<IdentifierNameSyntax>(options, IsFieldOrPropertyReference);
     }
 
     public IEnumerable<ReferenceInstance> Calculate(VariableDeclaratorSyntax options)
     {
-        SemanticModel semanticModel = compilation.GetSemanticModel(options.SyntaxTree);
-        ISymbol? variableSymbol = semanticModel.GetDeclaredSymbol(options);
-
-        if (variableSymbol == null)
-        {
-            return [];
-        }
-
-        List<ReferenceInstance> references = [];
-
-        foreach (SyntaxTree tree in compilation.SyntaxTrees)
-        {
-            SemanticModel treeModel = compilation.GetSemanticModel(tree);
-            SyntaxNode root = tree.GetRoot();
-
-            IEnumerable<IdentifierNameSyntax> identifiers = root.DescendantNodes()
-                .OfType<IdentifierNameSyntax>();
-
-            foreach (IdentifierNameSyntax identifier in identifiers)
-            {
-                ISymbol? identifierSymbol = treeModel.GetSymbolInfo(identifier).Symbol;
-
-                if (SymbolEqualityComparer.Default.Equals(identifierSymbol, variableSymbol))
-                {
-                    references.Add(CreateReference(identifier));
-                }
-            }
-        }
-
-        return references;
+        return BaseCalculate<IdentifierNameSyntax>(options, IsFieldOrPropertyReference);
     }
-    
-    private ReferenceInstance CreateReference(CSharpSyntaxNode reference)
+
+    private static bool IsMethodReference(
+        InvocationExpressionSyntax invocation,
+        SemanticModel treeModel,
+        ISymbol? memberSymbol)
     {
-        return new ReferenceInstance
-        {
-            Namespace = _namespaceCreator.CreateJoined(reference),
-            LineNumber = reference.GetLocation().GetLineSpan().StartLinePosition.Line + 1,
-            ColumnNumber = reference.GetLocation().GetLineSpan().StartLinePosition.Character + 1
-        };
+        SymbolInfo invocationInfo = treeModel.GetSymbolInfo(invocation);
+        ISymbol? invocationSymbol = invocationInfo.Symbol ?? invocationInfo.CandidateSymbols.FirstOrDefault();
+        
+        return invocationSymbol is IMethodSymbol invokedMethod
+            && SymbolEqualityComparer.Default.Equals(
+                invokedMethod.OriginalDefinition, memberSymbol?.OriginalDefinition);
+    }
+
+    private static bool IsFieldOrPropertyReference(
+        IdentifierNameSyntax identifier,
+        SemanticModel treeModel,
+        ISymbol? memberSymbol)
+    {
+        ISymbol? treeIdentifierSymbol = treeModel.GetSymbolInfo(identifier).Symbol;
+        return SymbolEqualityComparer.Default.Equals(treeIdentifierSymbol, memberSymbol);
     }
 }
